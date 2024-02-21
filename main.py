098211cb28fc5006a1fb4d57e1a6d5d988a9a599
Pavel <ya.skillet-02@ya.ru>
2024-02-21 14:15:38 +0300

    Добавлена база данных

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import asyncio
 
 import requests
 from telebot import types
-from telebot import asyncio_filters
+from telebot import asyncio_filters  # , asyncio_helper
 from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_storage import StateMemoryStorage
 from telebot.asyncio_handler_backends import State, StatesGroup
 from dotenv import load_dotenv
 
+from db import BotDB
 from validators import validate_game_url, validate_profile_url
 from parsers import get_game_id_from_name, get_player_name_and_id_from_url
 
@@ -19,26 +20,15 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 MY_WEB_API_STEAM_KEY = os.getenv('MY_WEB_API_STEAM_KEY')
 
-# GAME_VERDICTS = {
-#     'Counter-Strike 2': 'играет в Counter-Strike 2',
-# }
-
-GAME_VERDICTS = {}
-# steam_accounts = {'Вурдалак': '76561198804859278',
-#                   '1mpalions': '76561198307436325',
-#                   'D1vens': '76561198840271910',
-#                   'VICTORINO DE SPASITTO': '76561198440426454'
-#                   }
-
-steam_accounts = {}
 
 bot = AsyncTeleBot(TELEGRAM_TOKEN, state_storage=StateMemoryStorage())
+
+BotDB = BotDB('tg_steam_accs.db')
+
 
 support_games = {'Counter-Strike 2': '730', 'Dota 2': '570',
                  'PUBG: BATTLEGROUNDS': '578080', 'Palworld': '1623730',
                  'Apex Legends™': '1172470', 'Rust': '252490'}
-
-game_tracking = {}
 
 
 class GameStates(StatesGroup):
@@ -56,18 +46,10 @@ def get_api_answer(steamID):
     return api_data
 
 
-def parse_status(nickname, game):
-    if GAME_VERDICTS.get(game):
-        verdict = GAME_VERDICTS[game]
-    else:
-        verdict = None
-    return f'{nickname} {verdict}'
-
-
 class Treaker:
     work = False
 
-    async def start_spam(self, chat_id, markup):
+    async def start_spam(self, chat_id, user_id, markup):
         verdict_list = {
             'error': None,
         }
@@ -75,12 +57,12 @@ class Treaker:
                                reply_markup=markup)
         while self.work:
             try:
-                for nickname, steamID in steam_accounts.items():
+                for nickname, steamID in BotDB.get_all_track_users(user_id):
                     api_response = get_api_answer(steamID)
                     profile_status = api_response.get('personastate')
                     current_game = api_response.get('gameextrainfo')
                     if profile_status == 1 and current_game is not None:
-                        message = parse_status(nickname, current_game)
+                        message = f'{nickname} играет в {current_game}'
                         if (verdict_list.get(nickname) != message
                            and message.split()[1] != 'None'):
                             await bot.send_message(chat_id, message)
@@ -95,7 +77,6 @@ class Treaker:
                     print(message)
             else:
                 verdict_list['error'] = None
-                print("Ожидаем 5 секунд")
             await asyncio.sleep(5)
 
 
@@ -103,7 +84,7 @@ class Treaker:
 async def add_myself_game(message):
     markup = types.ReplyKeyboardRemove()
 
-    msg = ('Отправь ссылку на игру в стиме')
+    msg = ('Отправте ссылку на игру в стиме')
     await bot.set_state(message.from_user.id, GameStates.game_url,
                         message.chat.id)
     await bot.send_message(message.chat.id, msg,
@@ -113,6 +94,7 @@ async def add_myself_game(message):
 @bot.message_handler(state=GameStates.game_url)
 async def save_game_url(message):
     chat_id = message.chat.id
+    user_id = message.from_user.id
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     add_one_more = types.KeyboardButton('Добавить еще одну')
     menu = types.KeyboardButton('В меню')
@@ -122,13 +104,12 @@ async def save_game_url(message):
         steam_url = message.text
         game_name = steam_url.split('/')[-2]
         game_id = steam_url.split('/')[-3]
-        if game_id not in game_tracking:
-            game_tracking[game_id] = game_name
+        if (not BotDB.is_game_tracking(user_id, game_id)):
+            BotDB.add_game_to_track(user_id, game_id, game_name)
             await bot.send_message(chat_id,
                                    f'{game_name} успешно добалвена'
                                    ' в отслеживаемые',
                                    reply_markup=markup)
-            print(game_tracking)
         else:
             await bot.send_message(chat_id, 'Эта игра уже отслеживается',
                                    reply_markup=markup)
@@ -147,7 +128,7 @@ async def add_friend_profile(message):
 
     await bot.set_state(message.from_user.id, ProfileStates.profile_url,
                         chat_id)
-    await bot.send_message(chat_id, 'Отправь ссылку на '
+    await bot.send_message(chat_id, 'Отправьте ссылку на '
                                     'профиль игрока стим',
                            reply_markup=markup)
 
@@ -155,20 +136,21 @@ async def add_friend_profile(message):
 @bot.message_handler(state=ProfileStates.profile_url)
 async def save_profile_url(message):
     chat_id = message.chat.id
+    user_id = message.from_user.id
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     add_one_more = types.KeyboardButton('Добавить еще одного')
     menu = types.KeyboardButton('В меню')
     markup.add(add_one_more, menu)
 
     if validate_profile_url(message.text):
-        for name, id in get_player_name_and_id_from_url(message.text):
-            if name not in steam_accounts:
-                steam_accounts[name] = id
+        for username, steam_id in get_player_name_and_id_from_url(
+                message.text):
+            if (not BotDB.is_user_tracking(user_id, steam_id)):
+                BotDB.add_user_to_track(user_id, steam_id, username)
                 await bot.send_message(chat_id,
-                                       f'Игрок {name} добавлен '
+                                       f'Игрок {username} добавлен '
                                        'в отслеживаемые',
                                        reply_markup=markup)
-                print(steam_accounts)
             else:
                 await bot.send_message(chat_id, 'Этот игрок уже отслеживается',
                                        reply_markup=markup)
@@ -186,6 +168,11 @@ async def start(message):
     menu = types.KeyboardButton('В меню')
     markup.add(menu)
 
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name
+    if (not BotDB.user_exists(user_id)):
+        BotDB.add_user(user_id, first_name)
+
     msg = ('Привет, этот бот создан для того, чтобы присылать тебе '
            'уведомление, когда твои друзья заходят в игры стима. '
            'Ты можешь гулять, работать или просто валяться на '
@@ -198,6 +185,7 @@ async def start(message):
 @bot.message_handler(content_types=['text'])
 async def bot_message(message):
     chat_id = message.chat.id
+    user_id = message.from_user.id
     if message.chat.type == 'private':
         if message.text == 'В меню':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -246,12 +234,9 @@ async def bot_message(message):
             soobshenie = f'{message.text} добавлен(а) в отслеживаемые игры'
             game_id = support_games[message.text]
 
-            if game_id not in game_tracking:
+            if (not BotDB.is_game_tracking(user_id, game_id)):
                 game_id = get_game_id_from_name(message.text)
-                game_tracking[game_id] = message.text
-                GAME_VERDICTS[message.text] = f'играет в {message.text}'
-                print(game_tracking)
-                print(GAME_VERDICTS)
+                BotDB.add_game_to_track(user_id, game_id, message.text)
                 await bot.send_message(chat_id, soobshenie,
                                        reply_markup=markup)
             else:
@@ -259,9 +244,9 @@ async def bot_message(message):
                                        reply_markup=markup)
 
         elif message.text == 'Удалить игру':
-            if game_tracking:
+            if BotDB.get_all_user_games(user_id):
                 btns = []
-                for id, game in game_tracking.items():
+                for id, game in BotDB.get_all_user_games(user_id):
                     btns.append(types.InlineKeyboardButton(text=f'❌{game}',
                                                            callback_data=id))
                 markup = types.InlineKeyboardMarkup()
@@ -275,8 +260,9 @@ async def bot_message(message):
                                        'У вас нет отслеживаемых игр')
 
         elif message.text == 'Мои игры':
-            if game_tracking:
-                msg = ''.join(f'✅{game}\n' for game in game_tracking.values())
+            if BotDB.get_all_user_games(user_id):
+                msg = ''.join(f'✅{game[1]}\n' for game
+                              in BotDB.get_all_user_games(user_id))
                 await bot.send_message(chat_id, msg)
             else:
                 await bot.send_message(chat_id, 'У вас нет отслеживаемых игр')
@@ -286,7 +272,8 @@ async def bot_message(message):
             stop_track = types.KeyboardButton('Прекратить отслеживане')
             markup.add(stop_track)
             Treaker.work = True
-            asyncio.create_task(Treaker.start_spam(Treaker, chat_id, markup))
+            asyncio.create_task(Treaker.start_spam(Treaker, chat_id,
+                                                   user_id, markup))
 
         elif message.text == 'Прекратить отслеживане':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -297,17 +284,19 @@ async def bot_message(message):
                                    reply_markup=markup)
 
         elif message.text == 'Мои игроки':
-            if steam_accounts:
-                msg = ''.join(f'✅{acc}\n' for acc in steam_accounts.keys())
+            track_users = BotDB.get_all_track_users(user_id)
+            if len(track_users):
+                msg = ''.join(f'✅{acc[0]}\n' for acc in track_users)
                 await bot.send_message(chat_id, msg)
             else:
                 await bot.send_message(chat_id,
                                        'У вас нет отслеживаемых игроков')
 
         elif message.text == 'Удалить игрока':
-            if steam_accounts:
+            track_users = BotDB.get_all_track_users(user_id)
+            if len(track_users):
                 btns = []
-                for name, id in steam_accounts.items():
+                for name, id in track_users:
                     btns.append(types.InlineKeyboardButton(text=f'❌{name}',
                                                            callback_data=id))
                 markup = types.InlineKeyboardMarkup()
@@ -322,23 +311,25 @@ async def bot_message(message):
 
 
 @bot.callback_query_handler(func=lambda call: True)
-async def delete_game(id):
-    if id.data in game_tracking:
-        del_game = game_tracking.pop(id.data)
+async def delete_game_or_user(id):
+    user_id = id.from_user.id
+    if BotDB.is_game_tracking(user_id, id.data):
+        game_name = BotDB.get_game_name_by_id(user_id, id.data)
+        BotDB.remove_game_from_track(user_id, id.data)
         await bot.send_message(
             chat_id=id.message.chat.id,
-            text=f'{del_game} удалена из отслеживания')
-    elif id.data in steam_accounts.values():
-        invert_steam_accs = {j: i for i, j in steam_accounts.items()}
-        ids = invert_steam_accs[id.data]
-        del steam_accounts[ids]
+            text=f'{game_name} удалена из отслеживания')
+    elif BotDB.is_user_tracking(user_id, id.data):
+        user = BotDB.get_nickname_by_steam_id(user_id, id.data)
+        BotDB.remove_user_from_track(user_id, id.data)
         await bot.send_message(
             chat_id=id.message.chat.id,
-            text=f'{ids} удален(a) из отслеживания')
+            text=f'{user} удален(a) из отслеживания')
 
 
 bot.add_custom_filter(asyncio_filters.StateFilter(bot))
 bot.add_custom_filter(asyncio_filters.IsDigitFilter())
 
+# asyncio_helper.proxy = 'http://proxy.server:3128'
 
 asyncio.run(bot.polling())
--- temp.py
+++
@@ -1,8 +0,0 @@
-a = {'a': 'cjjfjb'}
-
-if 'cjjfjb' in a.values():
-    b = {val: key for key, val in a.items()}
-    print(b)
-    print('true')
-else:
-    print('false')
